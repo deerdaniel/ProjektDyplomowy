eb3a646b16d9dd6ae191ebd4ab8f8b507c21a4b7
Daniel <93049283+deerdaniel@users.noreply.github.com>
2025-01-13 18:42:05 +0100

    Add Third Person Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float smoothSpeed = 0.125f;
+    float rotationSpeed = 5f;
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(player);
+    }
+}
