cc0d7cb08f9ade393b11b34f9a9007546888f003
Daniel <93049283+deerdaniel@users.noreply.github.com>
2025-01-15 23:22:49 +0100

    Add navmesh

--- Assets/Scripts/StateMachine/States/EnemyFollowState.cs
+++ Assets/Scripts/StateMachine/States/EnemyFollowState.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyFollowState : EnemyBaseState
 {
     GameObject player;
     float distance;
+    private NavMeshAgent agent;
     public override void EnterState(EnemyStateMachine enemy)
     {
+        
     }
     public override void EnterState(EnemyStateMachine enemy, GameObject obj)
     {
+        agent = enemy.GetComponent<NavMeshAgent>();
         player = obj;
     }
     public override void UpdateState(EnemyStateMachine enemy)
     {
-        enemy.transform.LookAt(player.transform.position);
-        //enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, player.transform.position, enemy.RunSpeed * Time.deltaTime);
-        distance = Vector3.Distance(enemy.transform.position, player.transform.position);
-        if (0.7f > distance)
-        {
-            enemy.SwitchState(enemy.EnemyAttact);
-        }
+        agent.stoppingDistance = 2.0f;
+        agent.destination = player.transform.position;
+        //enemy.transform.LookAt(player.transform.position);
+        ////enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, player.transform.position, enemy.RunSpeed * Time.deltaTime);
+        //distance = Vector3.Distance(enemy.transform.position, player.transform.position);
+        //if (2.0f > distance)
+        //{
+        //    enemy.SwitchState(enemy.EnemyAttact);
+        //}
 
     }
     public override void OnCollisionEnter(EnemyStateMachine enemy, Collision collision)
